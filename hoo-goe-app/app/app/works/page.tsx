8952e002b3cdc81ea675e2fcef52be389d5d929c
SangyeopNam <spearoad15@gmail.com>
2024-08-25 14:48:33 +0900

    feat: works 포맷 변경, 새 작업 추가(7개)

--- hoo-goe-app/app/app/works/page.tsx
+++ hoo-goe-app/app/app/works/page.tsx
@@ -3,15 +3,24 @@ import Works, {Work} from "./works";
 
 export default function Page() {
   const works: Work[] = [
-    new Work(0, "[TVING] 썸머 러브머신 블루스", "사운드 디자인 ・ 폴리", "/images/works/1_TVING.jpeg", "png"),
-    new Work(1, "[TVING] 아일랜드", "사운드 디자인 ・ 폴리", "/images/works/2_TVING.jpeg", "png"),
-    new Work(2, "[POSCO] 마음도 택배가 되나요? ", "사운드 디자인 ・ 선곡", "/images/works/3_POSCO.webp", "png"),
-    new Work(3, "[KB국민카드] 엄지윤맘대로", "사운드 디자인 ・ 선곡", "/images/works/4_KB.webp", "png"),
-    new Work(4, "[JYP] NIMXX 쮸뀨미 시리즈", "사운드 디자인 ・ 선곡", "/images/works/5_JYP.jpg", "png"),
-    new Work(5, "[JEI 재능 TV] 쫑알쫑알 똘똘이", "사운드 디자인 ・ 폴리", "/images/works/6_JEI.jpg", "png"),
-    new Work(6, "[blip] '육각형 프로젝트' YOUNG POSSE 리뷰", "사운드 디자인 ・ 폴리", "/images/works/7_blip.webp", "png"),
-    new Work(7, "[CNN] 다큐멘터리 Across", "사운드 ・ 뮤직 슈퍼바이징", "/images/works/8_CNN.webp", "png"),
-    new Work(8, "[KB국민카드] 레고랜드 매니아 카드", "사운드 디자인", "/images/works/9_KB.webp", "png"),
+    new Work(0, "TVING", "썸머 러브머신 블루스", "사운드 디자인 ・ 폴리", "jpeg"),
+    new Work(1, "TVING", "아일랜드", "사운드 디자인 ・ 폴리", "jpeg"),
+    new Work(2, "POSCO", "마음도 택배가 되나요? ", "사운드 디자인 ・ 선곡", "webp"),
+    new Work(3, "KB국민카드", "엄지윤맘대로", "사운드 디자인 ・ 선곡", "webp"),
+    new Work(4, "JYP", "NIMXX 쮸뀨미 시리즈", "사운드 디자인 ・ 선곡", "jpg"),
+    new Work(5, "JEI 재능 TV", "쫑알쫑알 똘똘이", "사운드 디자인 ・ 폴리", "jpg"),
+    new Work(6, "blip", "'육각형 프로젝트' YOUNG POSSE 리뷰", "사운드 디자인 ・ 폴리", "webp"),
+    new Work(7, "CNN", "다큐멘터리 Across", "사운드 ・ 뮤직 슈퍼바이징", "webp"),
+    new Work(8, "KB국민카드", "레고랜드 매니아 카드", "사운드 디자인", "webp"),
+    new Work(9, "밤부네트워크", "네맛대로하는연애", "사운드 · 뮤직 슈퍼바이징"),
+    new Work(10, "삼성물산", "스타일뮤즈 X 숏필름", "사운드 디자인 · 선곡"),
+    new Work(11, "안다르", "애슬레저 안다르 맨즈", "사운드 디자인"),
+    new Work(12, "채널A", "여름아 부탁해", "사운드 디자인 · 폴리"),
+    new Work(13, "DINGO", "해방택시 시리즈", "사운드 디자인 · 선곡", "webp"),
+    new Work(14, "KB국민카드", "지들맘대로 손동표 시리즈", "사운드 디자인 · 선곡", "jpg"),
+    new Work(15, "KBS", "반지의 비밀일기", "사운드 디자인 · 폴리", "webp"),
+    new Work(16, "KOCCA", "오르는가요", "사운드 디자인 · 선곡", "jpg"),
+    new Work(17, "ZIGZAG", "Z ROOM", "사운드 디자인 · 선곡", "webp"),
   ];
   return (
     <main className="h-[60vh] overflow-scroll">
--- hoo-goe-app/app/app/works/works.tsx
+++ hoo-goe-app/app/app/works/works.tsx
@@ -5,32 +5,48 @@ import ReactPlayer from "react-player";
 
 export class Work {
   index: number;
+  client: string;
   title: string;
-  description: string;
+  position: string;
   url: string;
+  extension: string;
   workType: string;
 
   /**
-   * @param workType : 'youtube' : 유튜브 링크로 제공되는 작업, 'mp4' : 동영상 파일로 제공되는 작업
+   * @param workType : 'youtube' : 유튜브 링크로 제공, 'mp4' : 동영상 파일로 제공, 'image' : 이미지 사진만 제공
    */
-  constructor(index: number, title: string, description: string, url: string, workType: string = "youtube") {
+  constructor(
+    index: number,
+    client: string,
+    title: string,
+    position: string,
+    extension: string = "png",
+    workType: string = "image",
+    url: string = ""
+  ) {
     this.index = index;
+    this.client = client;
     this.title = title;
-    this.description = description;
+    this.position = position;
     this.url = url;
+    this.extension = extension;
     this.workType = workType;
+    if (workType == "image") this.url = `/images/works/${index}.${extension}`;
   }
 }
 
 export default function Works({works}: {works: Work[]}) {
   const workDivs = works.map((w: Work) => {
     switch (w.workType) {
-      case "youtube":
+      case "youtube": // 유튜브 링크
         return (
-          <div key={w.index} className="basis-1/4 ">
+          <div key={w.index} className="basis-1/4">
             <ReactPlayer url={w.url} width="100%" height="100%" />
-            <p className="text-center text-[14px] text-[#212121] mt-2">{w.title}</p>
-            <p className="text-center text-[12px] text-[#888888]">{w.description}</p>
+            <p className="text-center text-[14px] text-[#212121] mt-2">
+              <strong>[{w.client}]</strong>
+              {w.title}
+            </p>
+            <p className="text-center text-[12px] text-[#888888]">{w.position}</p>
           </div>
         );
       case "mp4": // 나중에 video 태그로 변경예정
@@ -39,18 +55,24 @@ export default function Works({works}: {works: Work[]}) {
             <Link href={w.url}>
               <div className="h-32 w-40 bg-slate-500"></div>
             </Link>
-            <p className="text-center text-[14px] text-[#212121] mt-2">{w.title}</p>
-            <p className="text-center text-[12px] text-[#888888]">{w.description}</p>
+            <p className="text-center text-[14px] text-[#212121] mt-2">
+              <strong>[{w.client}]</strong>
+              {w.title}
+            </p>
+            <p className="text-center text-[12px] text-[#888888]">{w.position}</p>
           </div>
         );
-      case "png":
+      case "image":
         return (
           <div key={w.index} className="min-w-[25%] max-w-[33%]">
             <div className="basis-1/4 aspect-video" style={{position: "relative"}}>
               <Image src={w.url} alt={w.title} layout="fill" />
             </div>
-            <p className="text-center text-[14px] text-[#212121] mt-2">{w.title}</p>
-            <p className="text-center text-[12px] text-[#888888]">{w.description}</p>
+            <p className="text-center text-[14px] text-[#212121] mt-2">
+              <strong>[{w.client}]</strong>
+              &nbsp; {w.title}
+            </p>
+            <p className="text-center text-[12px] text-[#888888]">{w.position}</p>
           </div>
         );
     }
