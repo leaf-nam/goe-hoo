420a414dd323640381912ca1098b30bafccfd94a
SangyeopNam <spearoad15@gmail.com>
2024-11-03 08:41:27 +0900

    fix: base image 수정

--- hoo-goe-app/app/page.tsx
+++ hoo-goe-app/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <div className="md:hidden h-[50px] w-full bg-white"></div>
         <div className="flex flex-col justify-center">
           <a className="mx-auto" href="#" onClick={() => alert("죄송합니다. 해당 페이지는 공사중입니다!")}>
-            <Image src={"/images/main/002.png"} alt="House of Ong.Al Base Image" width="400" height="300"></Image>
+            <Image src={"/images/main/002.png"} alt="House of Ong.R Base Image" width="400" height="300"></Image>
           </a>
           <div className="h-[20px]"></div>
           <h2 className="text-center text-xl font-['Kirang_Haerang'] text-[#020202]">ARCHIVE</h2>
