ad1f828378efe806cecc68bf99328fb933d845fb
SangyeopNam <spearoad15@gmail.com>
2025-08-07 17:24:32 +0900

    fix : houseofongr -> chaerok 변경

--- hoo-goe-app/app/components/logo.tsx
+++ hoo-goe-app/app/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div className="flex items-center p-7 ps-8">
-      <Image src={"/images/logo.png"} alt="House of Ong.Al Base Image" width="50" height="50"></Image>
+      <Image src={"/images/logo.png"} alt="Chaerok Base Image" width="50" height="50"></Image>
       {/* <div className="md:text-l font-sans font-black tracking-tighter">
         <p>HOUSE</p>
         <p>OF</p>
